5570c36d0788c4dc3b13c5ffe7b6daea9cb1fed3
Safg13 <58328621+Safg13@users.noreply.github.com>
2022-07-06 14:38:12 +0300

    v0.03
    Added 7 week cycles and some drugs

--- src/dhas/diary/java/Main.java
+++ src/dhas/diary/java/Main.java
@@ -11,6 +11,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+
 import org.apache.commons.io.output.TeeOutputStream;
 
 public class Main {
@@ -20,13 +21,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String inputedDateOfHospitalisation = scanner.nextLine();
 
-        System.out.println("Выберите препарат, 1 - Доксорубицин, 2 - Имуронвак ");
+        System.out.println("Выберите препарат, 1 - Доксорубицин, 2 - Имуронвак, 3 - Доцетаксел, 4 - Трипторелин ");
         Scanner scannerDrug = new Scanner(System.in);
         int drugChoice = scannerDrug.nextInt();
 
-        if (drugChoice != 1 && drugChoice != 2) {
+        if (drugChoice != 1 && drugChoice != 2 && drugChoice != 3 && drugChoice != 4) {
             System.out.println("Ошибка при выборе препарата");
         }
+
+        System.out.println("Выберите продолжительность схемы 1, 7 или 14 дней ");
+        Scanner scannerChemotherapyDuration = new Scanner(System.in);
+        int ChemotherapyDuration = scannerChemotherapyDuration.nextInt();
 
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         OutputStream teeStream = new TeeOutputStream(System.out, buffer);
@@ -39,13 +44,26 @@ public class Main {
         c.setTime(dateOfWeekToDateParser);
         int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
 
-        switch (dayOfWeek) {
-            case 2 -> IntravesicalScheme.getSchemeMonday14(inputedDateOfHospitalisation, drugChoice);
-            case 3 -> IntravesicalScheme.getSchemeTuesday14(inputedDateOfHospitalisation, drugChoice);
-            case 4 -> IntravesicalScheme.getSchemeWednesday14(inputedDateOfHospitalisation, drugChoice);
-            case 5 -> IntravesicalScheme.getSchemeThursday14(inputedDateOfHospitalisation, drugChoice);
-            case 6 -> IntravesicalScheme.getSchemeFriday14(inputedDateOfHospitalisation, drugChoice);
-            case 1, 7 -> System.out.println("Вы ввели дату выходного дня.");
+        if (ChemotherapyDuration == 1) {
+            ChemotherapyScheme.getScheme1(inputedDateOfHospitalisation, drugChoice);
+        } else if (ChemotherapyDuration == 7) {
+            switch (dayOfWeek) {
+                case 2 -> ChemotherapyScheme.getSchemeMonday7(inputedDateOfHospitalisation, drugChoice);
+                case 3 -> ChemotherapyScheme.getSchemeTuesday7(inputedDateOfHospitalisation, drugChoice);
+                case 4 -> ChemotherapyScheme.getSchemeWednesday7(inputedDateOfHospitalisation, drugChoice);
+                case 5 -> ChemotherapyScheme.getSchemeThursday7(inputedDateOfHospitalisation, drugChoice);
+                case 6 -> ChemotherapyScheme.getSchemeFriday7(inputedDateOfHospitalisation, drugChoice);
+                case 1, 7 -> System.out.println("Вы ввели дату выходного дня.");
+            }
+        } else if (ChemotherapyDuration == 14) {
+            switch (dayOfWeek) {
+                case 2 -> ChemotherapyScheme.getSchemeMonday14(inputedDateOfHospitalisation, drugChoice);
+                case 3 -> ChemotherapyScheme.getSchemeTuesday14(inputedDateOfHospitalisation, drugChoice);
+                case 4 -> ChemotherapyScheme.getSchemeWednesday14(inputedDateOfHospitalisation, drugChoice);
+                case 5 -> ChemotherapyScheme.getSchemeThursday14(inputedDateOfHospitalisation, drugChoice);
+                case 6 -> ChemotherapyScheme.getSchemeFriday14(inputedDateOfHospitalisation, drugChoice);
+                case 1, 7 -> System.out.println("Вы ввели дату выходного дня.");
+            }
         }
         //сохраняем buffer в файл
         try (OutputStream fileStream = new FileOutputStream("diary.txt")) {
