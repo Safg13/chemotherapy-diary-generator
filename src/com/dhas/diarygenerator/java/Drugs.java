c90249edd444c0174e7dd45eace8fb9d725e18c2
mev <rtrickz@gmail.com>
2023-11-24 18:00:15 +0300

    v0.855 Оптимизирован генератор дневника

--- src/com/dhas/diarygenerator/java/Drugs.java
+++ src/com/dhas/diarygenerator/java/Drugs.java
@@ -1,26 +1,28 @@
 package com.dhas.diarygenerator.java;
 
 public enum Drugs {
-    DOXORUBICINE("Пациент получил курс внутрипузырной терапии препаратом Доксорубицин 50 мг в/п, экспозиция препарата в мочевом пузыре 2 часа, перенес удовлетворительно.", true, false),
-    BCG("Пациент получил курс внутрипузырной терапии препаратом Имурон-Вак 100 мг в/п, экспозиция препарата в мочевом пузыре 2 часа, перенес удовлетворительно.", true, false),
-    DOCETAXEL("Пациент получил курс ХТ препаратом Доцетаксел 75 мг/м2 в/в, перенес удовлетворительно", false, false),
-    CABAZITAXEL("Пациент получил курс ХТ препаратом Кабазитаксел 25 мг/м2 в/в, перенес удовлетворительно", false, false),
-    DEGARELIX("Пациент получил курс АДТ препаратом Дегареликс 80 мг п/к, перенес удовлетворительно", false, false),
-    GOSERELIN("Пациент получил курс АДТ препаратом Гозерелин 3.6 мг п/к, перенес удовлетворительно", false, false),
-    DOCETAXEL_DEGARELIX(DOCETAXEL.getDiaryEntry(), false, true),
-    DOCETAXEL_GOSERELIN(DOCETAXEL.getDiaryEntry(), false, true),
-    CABAZITAXEL_DEGARELIX(CABAZITAXEL.getDiaryEntry(), false, true),
-    DEGARELIX_DOCETAXEL(DEGARELIX.getDiaryEntry(), false, true),
-    DEGARELIX_CABAZITAXEL(DEGARELIX.getDiaryEntry(), false, true);
+    DOXORUBICINE("Пациент получил курс внутрипузырной терапии препаратом Доксорубицин 50 мг в/п, экспозиция препарата в мочевом пузыре 2 часа, перенес удовлетворительно.", true, false, null),
+    BCG("Пациент получил курс внутрипузырной терапии препаратом Имурон-Вак 100 мг в/п, экспозиция препарата в мочевом пузыре 2 часа, перенес удовлетворительно.", true, false, null),
+    DOCETAXEL("Пациент получил курс ХТ препаратом Доцетаксел 75 мг/м2 в/в, перенес удовлетворительно", false, false, null),
+    CABAZITAXEL("Пациент получил курс ХТ препаратом Кабазитаксел 25 мг/м2 в/в, перенес удовлетворительно", false, false, null),
+    DEGARELIX("Пациент получил курс АДТ препаратом Дегареликс 80 мг п/к, перенес удовлетворительно", false, false, null),
+    GOSERELIN("Пациент получил курс АДТ препаратом Гозерелин 3.6 мг п/к, перенес удовлетворительно", false, false, null),
+    DOCETAXEL_DEGARELIX(DOCETAXEL.getDiaryEntry(), false, true, DEGARELIX),
+    DOCETAXEL_GOSERELIN(DOCETAXEL.getDiaryEntry(), false, true, GOSERELIN),
+    CABAZITAXEL_DEGARELIX(CABAZITAXEL.getDiaryEntry(), false, true, DEGARELIX),
+    DEGARELIX_DOCETAXEL(DEGARELIX.getDiaryEntry(), false, true, DOCETAXEL),
+    DEGARELIX_CABAZITAXEL(DEGARELIX.getDiaryEntry(), false, true, CABAZITAXEL);
 
     private final String diary;
     private final boolean isIntraVesicalScheme;
     private final boolean isTwoDrugsScheme;
+    private final Drugs secondDrug;
 
-    Drugs(String diary, boolean isIntraVesicalScheme, boolean isTwoDrugsScheme) {
+    Drugs(String diary, boolean isIntraVesicalScheme, boolean isTwoDrugsScheme, Drugs secondDrug) {
         this.diary = diary;
         this.isIntraVesicalScheme = isIntraVesicalScheme;
         this.isTwoDrugsScheme = isTwoDrugsScheme;
+        this.secondDrug = secondDrug;
     }
 
     public String getDiaryEntry() {
@@ -33,5 +35,9 @@ public enum Drugs {
 
     public boolean getIsIntravesical() {
         return isIntraVesicalScheme;
+    }
+
+    public Drugs getSecondDrug() {
+        return secondDrug;
     }
 }
